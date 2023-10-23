7ac8209fc0937db344b59356b5bedbda1d4d9a2c
hy_0807 <awe080798@gmail.com>
2023-10-24 02:15:46 +0900

    create new unity scene
    train unity

---
+++ Unity3d/Assets/Script/BackgroundScroll.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+	public float speed;
+	public Transform[] backgrounds;
+
+	float lPosX = 0f;
+	float rPosX = 0f;
+	float xScreenHalfSize;
+	float yScreenHalfSize;
+
+	// Use this for initialization
+	void Start () {
+		yScreenHalfSize = Camera.main.orthographicSize;
+		xScreenHalfSize = yScreenHalfSize * Camera.main.aspect;
+
+		lPosX = -xScreenHalfSize;
+		rPosX = xScreenHalfSize * 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < backgrounds.Length; i++)
+        {
+			backgrounds[i].position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+
+			if(backgrounds[i].position.x < lPosX)
+            {
+				Vector3 nextPos = backgrounds[i].position;
+				nextPos = new Vector3(nextPos.x + rPosX, nextPos.y, nextPos.z);
+				backgrounds[i].position = nextPos;
+            }
+        }
+	}
+}
---
+++ Unity3d/Assets/Script/Train.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Train : MonoBehaviour {
+
+	float lPosX = 0f;
+	float rPosX = 0f;
+	float xScreenHalfSize;
+	float yScreenHalfSize;
+
+	// Use this for initialization
+	void Start () {
+
+		//player position setting
+		transform.position = new Vector2(-8f,-1f);
+
+		//camera height, width
+		yScreenHalfSize = Camera.main.orthographicSize;
+		xScreenHalfSize = yScreenHalfSize * Camera.main.aspect;
+
+		lPosX = -xScreenHalfSize;
+		rPosX = xScreenHalfSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey("d"))
+        {
+			if(transform.position.x < rPosX)
+            {
+				transform.Translate(new Vector2(0.5f, 0));
+            }
+		}
+        if (Input.GetKey("a"))
+        {
+			if (transform.position.x > lPosX)
+			{
+				transform.Translate(new Vector2(-0.5f, 0));
+			}
+		}
+        if (Input.GetKey("w"))
+        {
+			if(transform.position.y < 1.0)
+            {
+				transform.Translate(new Vector2(0, 1.0f));
+            }
+        }
+		if (Input.GetKey("s"))
+		{
+			if (transform.position.y > -3.0)
+			{
+				transform.Translate(new Vector2(0, -1.0f));
+			}
+		}
+	}
+}
