2b803ab45fdcd496bf6a448d44f3121358d781bd
Lee Hayoung <awe080798@gmail.com>
2023-12-09 21:37:10 +0900

    update main scene
    add game icon popup

---
+++ Unity3d/Assets/Script/ClickManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickManager : MonoBehaviour {
+
+	public GameObject GamePopup;
+	public GameObject ClosetPopup;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+			
+			if (hit.collider != null && hit.collider.name == "Game")
+			{
+				GamePopup.gameObject.SetActive(true);
+			}
+
+
+		}
+	}
+}
---
+++ Unity3d/Assets/Script/OnMouse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnMouse : MonoBehaviour {
+
+	public Sprite[] img;
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter(){
+		spriteRenderer.sprite = img[1];
+	}
+
+	void OnMouseExit(){
+		spriteRenderer.sprite = img[0];
+	}
+}
